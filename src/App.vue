b7eac2a4ec3c128ca5c6424347a1eaf0c8a30428
oyatillohq <qurbonaliyev.com@gmail.com>
2024-07-21 07:22:03 +0500

    footer add

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <template>
   <main>
-    <div class="container">
-      <div class="nav">
+
+    <div class="nav">
+      <div class="container">
         <router-link to="/" class="logo">
           <img src="./assets/img/svg.svg" alt="" class="logo-svg">
           <h1 class="logo-title">Ibn Sino<span>Education</span></h1>
         </router-link>
         <ul class="nav-list">
-          <li class="nav-list__item"><a href="tel:+998948553366">Biz bilan bog'laning</a></li>
-          <li class="nav-list__item"><a href="#">Manzil</a></li>
           <li class="nav-list__item"><a href="#">Biz haqimizda</a></li>
           <li class="nav-list__item"><a href="#">O'qituvchilar</a></li>
           <li class="nav-list__item"><a href="#">Yutuqlar</a></li>
+          <li class="nav-list__item"><a href="tel:+998948553366">Biz bilan bog'laning</a></li>
+          <li class="nav-list__item"><a href="#">Manzil</a></li>
         </ul>
       </div>
-      <video controls class="banner"><source src="./assets/video/8qqam39vpufbm7jypern.mp4"></video>
     </div>
+    <router-view />
+    <div class="footer">
+      <div class="container">
+        <ul class="footer-list">
+          <li class="footer-list__item"><a href="tel:+998948553366">Biz bilan bog'laning</a></li>
+          <li class="footer-list__item"><a href="">Manzil</a></li>
+          <li class="footer-list__item"><a href="">Biz haqimizda</a></li>
+          <li class="footer-list__item"><a href="">O'qituvchilar</a></li>
+          <li class="footer-list__item"><a href="">Yutuqlar</a></li>
+        </ul>
+        <div class="social-network">
+          <div class="item">
+            <img src="./assets/img/telegram.png" alt="" class="item-img">
+            <a href="https://t.me/Ibn_Sino_Education" class="item-title">Telegram</a>
+          </div>
+          <div class="item">
+            <img src="./assets/img/instagram.png" alt="" class="item-img">
+            <a href="https://www.instagram.com/ibnsinoeducationofficial" class="item-title">Instagram</a>
+          </div>
+          <div class="item">
+            <img src="./assets/img/facebook.svg" alt="" class="item-img">
+            <router-link to="/" class="item-title">Facebook</router-link>
+          </div>
+          <div class="item">
+            <img src="./assets/img/you tube.png" alt="" class="item-img">
+            <a href="https://www.youtube.com/@ibnsinoeducation3090" class="item-title">You Tube</a>
+          </div>
+        </div>
+      </div>
+      <div class="uzb">O'zbekiston Respublikasi 2024©</div>
+    </div>
+
   </main>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-
+    <div class="banner">
+        <video controls class="banner-video"><source src="../assets/video/8qqam39vpufbm7jypern.mp4">Ibn sino</video>
+      </div>
   </div>
 </template>
 
